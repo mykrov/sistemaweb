36fd72583940057bc0269ecb8333dc0715ecbf36
ManuelLaptop <salvatorex89@gmail.com>
2016-05-01 12:26:14 -0400

    modelo de pruevas

--- registro/models.py
+++ registro/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class registropaciente(models.Models):
+	nombre = models.ChartField(max_length=30)
+	apellido = models.ChartField(max_length=30)
+	cedula = models.IntegerFiel(max_length=30)
+	telefono = models.IntegerFiel(max_length=30)
+	edad = models.ChartField(max_length=30)
+	sexo =  models.ChartField(max_length=10)
+	municipio = models.ChartField(max_length=30)
+	parroquia = models.ChartField(max_length=15)
+	urbarrio = models.ChartField(max_length=15)
+	direccion = models.ChartField(max_length=60)
--- registro/views.py
+++ registro/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def registrop (request):
+	return HttpResponse('Hola esto es el registro de un paciente')
